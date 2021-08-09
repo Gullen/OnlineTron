2d1cda5ccaeffb78fd4b84e36119107cdbbb70c8
Gullen <jaygullen2345@gmail.com>
2021-08-09 19:39:20 +0100

    Added test game state

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const gameScreen = document.getElementById('gameScreen');
 
 let canvas, ctx;
 
+const gameState = {
+    player : {
+        pos : {
+            x : 3,
+            y : 10,
+        },
+        vel : {
+            x : 1,
+            y : 0,
+        },
+        snake : [
+            {x: 1, y: 10},
+            {x: 2, y: 10},
+            {x: 3, y: 10},
+        ],
+    },
+    food: {
+        x: 7,
+        y: 7,
+    },
+    gridsize: 20,
+}
+
 function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
