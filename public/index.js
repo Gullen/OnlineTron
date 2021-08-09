8ae23252fb87bdba8369cc3689c21c9467565bef
Gullen <jaygullen2345@gmail.com>
2021-08-09 20:10:31 +0100

    deleted server files to restart

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
+
 const gameScreen = document.getElementById('gameScreen');
 
 let canvas, ctx;
@@ -68,3 +69,4 @@ function paintPlayer(playerState, size, colour){
 
 init();
 paintGame(gameState);
+
