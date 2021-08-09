77b889b0e439f9ae55bda070f62f9d3c37bbcdfd
Gullen <jaygullen2345@gmail.com>
2021-08-09 22:07:18 +0100

    added client server communication

--- private/gameAPI.js
+++ private/gameAPI.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { request, response } = require('express');
 
+let player1ready = false;
+let player2ready = false;
+
 module.exports = function api(game){
     game.use(express.json({limit: '2mb'}));
 
@@ -12,5 +15,25 @@ module.exports = function api(game){
         let melon = 32;
         const testData = {"melon" : melon};
         response.send(testData);
+    });
+
+    game.post('/playerReady', async (request, response) => {
+        const data = request.body;
+        if (data.player == 1){
+            player1ready = true;
+            console.log("PLAYER 1 READY");
+        }
+        else if (data.player == 2){
+            player2ready = true;
+            console.log("PLAYER 2 READY");
+        }
+        let status = "RECCEIVED - READY"
+        const dataToSend = {"status" : status, "player" : data.player, "player1ready" : player1ready, "player2ready" : player2ready};
+        response.send(dataToSend);
+        player1ready = true;
+    });
+
+    game.post('/updateState', async (request, response) => {
+        const reccievedState = request.body;
     });
 }
